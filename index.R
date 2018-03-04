533db9cf9382bfdaa46508c53a5ed2d1ffac6c4a
David Klein <davidklein@Davids-MacBook-Air-4.local>
2018-03-04 15:50:23 -0800

    reverts to correct index

--- index.R
+++ index.R
@@ -61,26 +61,6 @@ data <- flatten(proposals) %>%
   filter(published == TRUE) %>%
   filter(length(body.legacy) > 0) %>%
   select(title, year, quarter, asked, received, category, content = body.legacy)
-  # mutate(content = select(content, title))
-  # flatten(content)
-# mutate(content = attach(content))
-# mutate(content = as.data.frame(content))
-  # mutate(content = as.data.frame(do.call(rbind, content[[1]])))
-  # mutate(test = data.frame(unlist(body.legacy)))
-  # mutate(test = unnest(body.legacy))
-  # mutate(test = do.call(rbind, lapply(body.legacy, data.frame, stringsAsFactors=FALSE)))
-  # mutate(test = data.frame(matrix(unlist(body.legacy)),stringsAsFactors=FALSE))
-  # mutate(test = length(body.legacy))
-  # mutate(body = as.data.frame(body.legacy)) %>%
-  # mutate(content = data.frame(words = unlist(body.legacy)))
-  # mutate(content = do.call(rbind.data.frame, body.legacy))
-  # mutate(content = list(body.legacy$title, body.legacy$body))
-  # filter(length(body) > 0)
-# print(data)
-
-# test <- as.data.frame(data$body.legacy, check.names = FALSE, stringAsFactors = FALSE)
-# test <- data.frame(data$content, row.names = c('title','body'))
-# test <- select(data$content, title)
 for (i in length(data)) {
   View(data[[i]]$content)
   # test <- typeof(data[[i]])
